9237f8337b050cd412b3bd6f2891ef50611f3fc4
Wessi <kiyako1991@gmail.com>
2021-04-22 11:42:24 +0300

    Fix class duplication issue

--- src/main/java/etgov/mof/pfmrt/dao/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package etgov.mof.pfmrt.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import etgov.mof.pfmrt.model.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-    
-}
